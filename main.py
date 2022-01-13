9e674e794bc91b76ccf29dbbaa2a69028a903861
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-01-13 14:23:08 +0900

    Update dot env
    - 환경변수명 수정
    - user_id로 email 가져오는 함수 생성

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import os
 import pymysql
+from dotenv import load_dotenv
+load_dotenv()
 
 
-connection = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='1234',
-    db='test',
-    charset='utf8',
-)
-cursor = connection.cursor()
-sql = 'select * from test'
-cursor.execute(sql)
-data = cursor.fetchall()
-print(data)
+def get_email(user_id):
+    connection = pymysql.connect(
+        host=os.environ.get('MYSQL_HOST'),
+        user=os.environ.get('MYSQL_USER'),
+        password=os.environ.get('MYSQL_PASSWORD'),
+        db=os.environ.get('MYSQL_DB'),
+        charset='utf8',
+    )
+    cursor = connection.cursor()
+    sql = 'select user_email from member_email_encrypt where id = %s'
+    cursor.execute(sql, user_id)
+    return cursor.fetchone()[0]
+
+
+print(get_email(1))
