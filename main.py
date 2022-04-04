0aeabce5b9738ed2479ed6c627d9e038071e2de9
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-04-05 00:15:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,10 @@
-import os
-import re
-import pymysql
-import urllib.request
-from urllib.parse import quote
-from dotenv import load_dotenv
+from modules.common import *
+# from modules.email import *
 
-load_dotenv()
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
-
-def count_email():
-    sql = 'select count(*) from member_email_encrypt'
-    cursor.execute(sql)
-    return cursor.fetchone()[0]
-
-
-def get_email(user_id):
-    sql = 'select user_email from member_email_encrypt where id = %s'
-    cursor.execute(sql, user_id)
-    return cursor.fetchone()[0]
-
-
-def decrypt_email(email):
-    url = os.environ.get('DECRYPT_API_URL')
-    request = urllib.request.urlopen(url + quote(email)).read()
-    return request.decode('cp949')
-
-
-def update_email(email, user_id):
-    sql = 'update member_email_encrypt set user_email = %s where id = %s'
-    cursor.execute(sql, (email, user_id))
-    connection.commit()
-
-
-def check_email(email):
-    if not re.fullmatch(regex, email):
-        print(email)
-
-
-connection = pymysql.connect(
-    host=os.environ.get('MYSQL_HOST'),
-    user=os.environ.get('MYSQL_USER'),
-    password=os.environ.get('MYSQL_PASSWORD'),
-    db=os.environ.get('MYSQL_DB'),
-    charset='utf8',
-)
-cursor = connection.cursor()
-
-i = 1
-while i <= count_email():
-    encrypted_email = get_email(i)
-    # print('id: ', i, ', encrypt: ', encrypted_email)
-    decrypted_email = decrypt_email(encrypted_email)
-    # print('id: ', i, ', decrypt: ', decrypted_email)
-    check_email(decrypted_email)
-    update_email(decrypted_email, i)
-    i = i + 1
+for i in range(1, count_column()):
+    encrypted_data = get_data(i)
+    # print('id: ', i, ', encrypt: ', encrypted_data)
+    decrypted_data = decrypt_data(encrypted_data)
+    # print('id: ', i, ', decrypt: ', decrypted_data)
+    # check_email(decrypted_data)
+    update_data(decrypted_data, i)
