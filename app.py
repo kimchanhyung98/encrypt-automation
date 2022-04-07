a71d059d842b75c1b919045af96def2466fa78a1
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-04-07 14:25:03 +0900

    Update app.py
    - 변수가 none 인 경우
    - 미사용 함수 제거
    - 확인을 위해 print 추가

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from services.common import *
-# from services.email import *
+from services.email import *
 
 for i in range(1, count_column()):
     encrypted_data = get_data(i)
-    # print('id: ', i, ', encrypt: ', encrypted_data)
-    decrypted_data = decrypt_data(encrypted_data)
-    # print('id: ', i, ', decrypt: ', decrypted_data)
-    # check_email(decrypted_data)
-    update_data(decrypted_data, i)
+    print('id: ', i, ', encrypt: ', encrypted_data)
+
+    if encrypted_data is not None:
+        decrypted_data = decrypt_data(encrypted_data)
+        print('id: ', i, ', decrypt: ', decrypted_data)
+        update_data(decrypted_data, i)
